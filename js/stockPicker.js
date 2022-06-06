e4839a7d1a200e939843a2cec5a56d3aa510d121
afelix997 <afelix2013@gmail.com>
2022-06-06 12:44:42 -0700

    answer

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,11 @@
-exports.picker = function(prices) {
-
+exports.picker = function (prices) {
+    let result = []
+    let maxProfit=0
+    for (i = 0; i < prices.length; i++) {
+        for (k = i+1; k < prices.length; k++) {
+            if ((prices[k] - prices[i]) > maxProfit && prices[k] != prices[0] && prices[i] != prices[prices.length - 1]) {
+                maxProfit=prices[k] - prices[i]
+                result= [i,k]
+    }}}
+    return result
 }
