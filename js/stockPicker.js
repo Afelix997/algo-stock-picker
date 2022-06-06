b876c581fdeddfdf74facedab4ed6522963de532
afelix997 <afelix2013@gmail.com>
2022-06-06 13:00:24 -0700

    answer

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -3,9 +3,10 @@ exports.picker = function (prices) {
     let maxProfit=0
     for (i = 0; i < prices.length; i++) {
         for (k = i+1; k < prices.length; k++) {
-            if ((prices[k] - prices[i]) > maxProfit && prices[k] != prices[0] && prices[i] != prices[prices.length - 1]) {
+            if ((prices[k] - prices[i]) > maxProfit && i<k) {
                 maxProfit=prices[k] - prices[i]
                 result= [i,k]
     }}}
     return result
 }
+
--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,8 @@
 def picker(prices):
-    pass
+    max_profit=0
+    for i in range (0,len(prices)):
+        for k in range (1,len(prices)):
+            if prices[k] - prices[i] > max_profit and i<k:
+                max_profit=prices[k] - prices[i]
+                result= [i,k]
+    return result
